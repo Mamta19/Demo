91edc75311f073400c2470fab5ea176e0c1106cb
Mamta <mamta.kalyankar@gmail.com>
2017-11-07 19:31:10 -0600

    Demo

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function search(){
+    var site ="wikipedia.org";
+    var lookfor = document.getElementById("txtlookfor").value;
+    if(lookfor.length>0){
+    var query ="http://www.google.com/search?q=" + encodeURIComponent(lookfor) + " site:" + site;
+    location.href=query;
+}
+}
